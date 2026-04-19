a0def8054fd3e09acc4c7bdfaad60f9f3c0cda8e
restauradorofficina-hub <restauradorofficina@gmail.com>
2026-04-19 13:54:35 -0300

    feat: barra de navegação inferior com Buscar e Favoritos

--- 03_desenvolvimento/vodibus_app/lib/main.dart
+++ 03_desenvolvimento/vodibus_app/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'screens/splash_screen.dart';
+import 'screens/home_screen.dart';
+import 'screens/favoritos_screen.dart';
 
 void main() {
   runApp(const VoDiBusApp());
@@ -18,6 +20,40 @@ class VoDiBusApp extends StatelessWidget {
         useMaterial3: true,
       ),
       home: const SplashScreen(),
+    );
+  }
+}
+
+class MainScreen extends StatefulWidget {
+  const MainScreen({super.key});
+
+  @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  int _indiceAtual = 0;
+
+  final List<Widget> _telas = const [HomeScreen(), FavoritosScreen()];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: _telas[_indiceAtual],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _indiceAtual,
+        onTap: (index) {
+          setState(() {
+            _indiceAtual = index;
+          });
+        },
+        selectedItemColor: const Color(0xFF1565C0),
+        unselectedItemColor: Colors.grey,
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.search), label: 'Buscar'),
+          BottomNavigationBarItem(icon: Icon(Icons.star), label: 'Favoritos'),
+        ],
+      ),
     );
   }
 }
